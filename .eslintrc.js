a97da93123ca7da01352e043da1b6fd381093f4d
Steve Pietrek <spietrek@gmail.com>
2019-09-26 07:57:15 -0400

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,21 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
+  extends: [
     'plugin:vue/essential',
-    '@vue/standard'
+    '@vue/standard',
+    'prettier/vue',
+    'plugin:prettier/recommended',
   ],
   rules: {
+    'import/no-extraneous-dependencies': 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-vars': 'error',
   },
   parserOptions: {
-    parser: 'babel-eslint'
-  }
+    parser: 'babel-eslint',
+  },
 }
